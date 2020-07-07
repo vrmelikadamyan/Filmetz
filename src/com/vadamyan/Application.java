c1f875c614bbfeff24888ab422597d2ef04ed11e
Vadim Melik-Adamyan <vrmelikadamyan@gmail.com>
2020-07-07 20:29:54 +0300

    Initial commit

---
+++ src/com/vadamyan/Application.java
@@ -0,0 +1,4 @@
+package com.vadamyan;
+
+public class Application {
+}
---
+++ src/com/vadamyan/Filmetz.java
@@ -0,0 +1,8 @@
+package com.vadamyan;
+
+public class Filmetz {
+
+    public static void main(String[] args) {
+	Application app = new Application();
+    }
+}
